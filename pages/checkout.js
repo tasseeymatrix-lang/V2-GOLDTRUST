3237e4b6d5ebf81411d6188544b7e26a4b85a3c3
MATRIX <tasseeymatrix@gmail.com>
2026-02-07 09:06:30 +0100

    Update CODE_PRICE from 5500 to 7000

--- pages/checkout.js
+++ pages/checkout.js
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 import Layout from '../components/Layout';
 import { saveTx } from '../utils/storage';
 
-const CODE_PRICE = 5500;
+const CODE_PRICE = 7000;
 const WA = '+2348136347797';
 
 export default function Checkout() {
